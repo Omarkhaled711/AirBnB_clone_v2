3e7684c6dbab68d08b9af077ad9330e7b8b2de6f
Omar Khaled <o.khaled6789@gmail.com>
2023-10-06 02:18:55 +0300

    added 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# do stuff
+
+sudo apt update
+sudo apt install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+cat << EOT | sudo tee -a /data/web_static/releases/test/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOT
+
+if [ -L /data/web_static/current ]; then
+    # Delete the existing symbolic link
+    sudo rm /data/web_static/current
+fi
+
+# Create the new symbolic link
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+config_file="/etc/nginx/sites-available/default"
+sudo sed -i '/^server {/a \    location /hbnb_static {\n        alias /data/web_static/current/;\n    }\n' "$config_file"
+sudo service nginx restart
